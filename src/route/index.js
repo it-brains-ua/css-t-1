e569354ec682b4b862f109170185418fbdca2ad5
CLII412 <sw412fr@gmail.com>
2023-05-25 13:42:36 +0300

    init

--- src/route/index.js
+++ src/route/index.js
@@ -775,5 +775,20 @@ router.get('/template-5', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/slack', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('slack', {
+    layout: null,
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 // Підключаємо роутер до бек-енду
 module.exports = router
